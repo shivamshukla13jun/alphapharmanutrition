579a67b51501019a3e76d9b4940faffbaa559012
shivamshukla13jun <107310556+shivamshukla13jun@users.noreply.github.com>
2022-06-13 16:04:43 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-$conn=mysqli_connect('localhost','vrlpwvmy_fattofit','E^8?bo;K=A#(','vrlpwvmy_alphasameernutri') or die("404 Not Found");
+$conn=mysqli_connect('localhost','root','','vrlpwvmy_alphasameernutri') or die("404 Not Found");
 
 ?>
